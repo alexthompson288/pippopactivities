49feed394d306c6508cd0208015ab0ad166c6e02
Alex Thompson <alex@alexjbthompson.com>
2015-06-02 18:35:53 +0100

    Users can login and register with production server

--- pippopactivities/LoginController.swift
+++ pippopactivities/LoginController.swift
@@ -18,28 +18,152 @@ class LoginController:UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var PasswordField: UITextField!
     
-    var token:String = ""
-    var savedEmail = ""
-
+    @IBOutlet weak var RegisterChildField: UITextField!
     
-    override func viewDidLoad() {
-        println("Login controller")
-        savedEmail = NSUserDefaults.standardUserDefaults().objectForKey("email") as! String
-        println("Finding saved email from NSUserDefaults \(savedEmail)")
-        if savedEmail != "" {
+    @IBOutlet weak var RegisterParentField: UITextField!
+    
+    @IBOutlet weak var RegisterEmailField: UITextField!
+    
+    @IBOutlet weak var RegisterPasswordField: UITextField!
+    
+    @IBOutlet weak var LoginRegisterButtonLabel: UIButton!
+    
+    
+    var visible:CGFloat = 1.0
+    var invisible:CGFloat = 0.0
+    
+    
+    var loginScreen = true{
+        didSet{
+            updateUI()
+        }
+    }
+    var token:String = ""
+    var savedEmail:String?
+
+    override func viewDidAppear(animated: Bool) {
+
+        savedEmail = NSUserDefaults.standardUserDefaults().objectForKey("email") as? String
+        if let mySavedEmail = savedEmail {
             println("About to perform segue")
             performSegueWithIdentifier("LoginToActivitiesSegue", sender: self)
+            println("Finding saved email from NSUserDefaults \(mySavedEmail)")
         }
-        AttemptLoginWithLocalDetails()
+    }
+    
+    func updateUI(){
+        if self.loginScreen == true {
+            self.EmailField.alpha = visible
+            self.PasswordField.alpha = visible
+            self.RegisterChildField.alpha = self.invisible
+            self.RegisterParentField.alpha = self.invisible
+            self.RegisterEmailField.alpha = self.invisible
+            self.RegisterPasswordField.alpha = self.invisible
+            self.LoginRegisterButtonLabel.titleLabel!.text = "Login"
+        } else {
+            self.EmailField.alpha = self.invisible
+            self.PasswordField.alpha = self.invisible
+            self.RegisterChildField.alpha = visible
+            self.RegisterParentField.alpha = visible
+            self.RegisterEmailField.alpha = visible
+            self.RegisterPasswordField.alpha = visible
+            self.LoginRegisterButtonLabel.titleLabel!.text = "Register"
+
+        }
+    }
+    
+    override func viewDidLoad() {
+        self.loginScreen = true
+        println("Login controller")
+//        AttemptLoginWithLocalDetails()
+    }
+    
+    @IBAction func RegisterLoginToggleButton(sender: AnyObject) {
+        if self.loginScreen == true {
+            self.loginScreen = false
+        } else {
+            self.loginScreen = true
+        }
+    }
+    
+    func hideLoginItems(){
+        println("Need to hide login now...")
+    }
+    
+    func showLoginItems(){
+        println("Need to show login now...")
+    }
+    
+    func hideRegisterItems(){
+        println("Need to hide register now...")
+    }
+    
+    func showRegisterItems(){
+        println("Need to show register now...")
+
     }
     
     @IBAction func LoginButton(sender: AnyObject) {
+        if self.loginScreen == true {
+            FirstLoginUserFunction()
+        } else {
+            RegisterUser()
+        }
+    }
+    
+    func RegisterUser(){
+        if self.RegisterChildField == "" {
+            self.ErrorLabel.text = "Fill in name"
+        } else if self.RegisterParentField == ""{
+            self.ErrorLabel.text = "Fill in name"
+        }
+        else if self.RegisterEmailField == ""{
+            self.ErrorLabel.text = "Fill in email"
+        }
+        else if self.RegisterPasswordField == ""{
+            self.ErrorLabel.text = "Fill in password"
+        }
+        else {
+            RegisterUserRemote(self.RegisterChildField.text, parent: self.RegisterParentField.text, email: self.RegisterEmailField.text, password: self.RegisterPasswordField.text)
+        }
+    }
+    
+    func RegisterUserRemote(child: String, parent: String, email: String, password: String){
+        println("Registering child...about to hit server")
+        let url = NSURL(string: "http://www.pippoplearning.com/api/v3/users")!
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        request.HTTPBody = "{ \n    \"user\": {\n  \"learner_name\": \"\(child)\",\"name\": \"\(parent)\", \"email\": \"\(email)\",\"password\": \"\(password)\"\n}\n}".dataUsingEncoding(NSUTF8StringEncoding);
+        
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { (data: NSData!, response: NSURLResponse!, error: NSError!) in
+            
+            if error != nil {
+                // Handle error...
+                return
+            }
+            var responseObject:NSDictionary?
+            responseObject = self.dataToJSON(data)
+            if let jsonDict = responseObject {
+                println("SUCCESS.JSON Response is \(jsonDict)")
+            }
+            else{
+                println("problem in JSON")
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func FirstLoginUserFunction(){
         if self.EmailField == "" {
             self.ErrorLabel.text = "Fill in email"
         } else if self.PasswordField == ""{
             self.ErrorLabel.text = "Fill in password"
         } else {
-            logUserIn()
+            LogUserInRemote(self.EmailField.text, password: self.PasswordField.text)
         }
     }
 
@@ -65,17 +189,16 @@ class LoginController:UIViewController, UITextFieldDelegate {
     }
     
     func AttemptLoginWithLocalDetails(){
-        var email = ""
-        var password = ""
-        email = NSUserDefaults.standardUserDefaults().objectForKey("email") as! String
-        password = NSUserDefaults.standardUserDefaults().objectForKey("password") as! String
+        var email:String?
+        var password:String?
+        email = NSUserDefaults.standardUserDefaults().objectForKey("email") as? String
+        password = NSUserDefaults.standardUserDefaults().objectForKey("password") as? String
         println("Saved email is \(email) and password is \(password)")
-        if email != ""{
-            if password != ""{
-                LogUserInRemote(email, password: password)
-            }
+        if let validEmail = email {
+            println("Valid email")
         }
     }
+    
     
     func logUserIn(){
         var email = ""
